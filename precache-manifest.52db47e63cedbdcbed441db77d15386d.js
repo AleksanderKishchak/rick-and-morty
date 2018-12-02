424b666618f0f247e551087b3275994c38c83721
Aleksander Kishchak <sanya69876@gmail.com>
2018-12-03 00:34:28 +0200

    Updates

---
+++ precache-manifest.52db47e63cedbdcbed441db77d15386d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e8a6392fe1cd7f5726ac",
+    "url": "/rick-and-morty/static/js/runtime~main.e8a6392f.js"
+  },
+  {
+    "revision": "9baf358dbca5a91ed3be",
+    "url": "/rick-and-morty/static/js/main.9baf358d.chunk.js"
+  },
+  {
+    "revision": "48abcc24d4f333d1302e",
+    "url": "/rick-and-morty/static/js/1.48abcc24.chunk.js"
+  },
+  {
+    "revision": "9baf358dbca5a91ed3be",
+    "url": "/rick-and-morty/static/css/main.4f990a6b.chunk.css"
+  },
+  {
+    "revision": "8310e6ecff210df876721cf62c2c6630",
+    "url": "/rick-and-morty/index.html"
+  }
+];
